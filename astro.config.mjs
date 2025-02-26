da8278833a9a46b79fb4a9b9fa22ddb221bc839f
ronaldjames6548 <ronaldjames6548@gmail.com>
2025-02-26 18:43:09 +0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,24 +4,48 @@ import sitemap from "@astrojs/sitemap";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import { autolinkConfig } from "./plugins/rehype-autolink-config";
 import rehypeSlug from "rehype-slug";
+import { i18n, filterSitemapByDefaultLocale } from "astro-i18n-aut/integration";
 import astroI18next from "astro-i18next";
 import alpinejs from "@astrojs/alpinejs";
 import AstroPWA from "@vite-pwa/astro";
 import icon from "astro-icon";
 import solidJs from "@astrojs/solid-js";
-
 import vercel from "@astrojs/vercel/serverless";
+
+const defaultLocale = "en";
+const locales = {
+  en: "en", // the `defaultLocale` value must present in `locales` keys
+  it: "it",
+  fr: "fr",
+};
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astros.zank.studio",
+  site: "https://ytdll.vercel.app/it",
+  trailingSlash: "always",
+  build: {
+    format: "directory",
+  },
   vite: {
     define: {
       __DATE__: `'${new Date().toISOString()}'`
     }
   },
   output: "hybrid",
-  integrations: [tailwind(), sitemap(), astroI18next(), alpinejs(), AstroPWA({
+  integrations: [tailwind(), sitemap({
+      i18n: {
+        locales,
+        defaultLocale,
+      },
+      filter: filterSitemapByDefaultLocale({ defaultLocale }),
+    }),
+    astroI18next(), 
+    alpinejs(), 
+    i18n({
+      locales,
+      defaultLocale,
+    }),
+    AstroPWA({
     mode: "production",
     base: "/",
     scope: "/",
